96eb17bad00f66fa3b25a7b52ad8bef8b62d06fe
IvanRyaboy <93194467+IvanRyaboy@users.noreply.github.com>
2023-04-14 15:27:42 +0300

    change shape of all skills

--- djsite/coolsite/roadmap/admin.py
+++ djsite/coolsite/roadmap/admin.py
@@ -4,9 +4,8 @@ from .models import *
 
 
 class SkillAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'about', 'priority', 'links')
+    list_display = ('id', 'name', 'about', 'links')
     list_display_links = ('id', 'name')
-    search_fields = ('priority', 'name')
 
 
 admin.site.register(Skill, SkillAdmin)
